8353fb83799484e6b00905423ccea117ad3a1c66
BhushanShahare000 <bhushanshahare9284@gmail.com>
2024-06-14 17:13:52 +0530

    added music on page

--- src/components/social-icon/Social.jsx
+++ src/components/social-icon/Social.jsx
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Menu.css"; // Import your CSS file
-import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube, FaPlay, FaPause } from "react-icons/fa";
 import { BiSolidMessageSquareAdd } from "react-icons/bi";
 import Donate from "../../assets/images/donationsai.png";
+import backgroundMusic from "../../../src/audio/108_namavali.mp3";
 
 const Social = () => {
   const [isMenuVisible, setMenuVisible] = useState(true);
   const [isDonateVisible, setDonateVisible] = useState(true);
+  const [isMusicPlaying, setMusicPlaying] = useState(false); // Initialize music as paused
+
+  useEffect(() => {
+    const audio = new Audio(backgroundMusic);
+    
+    // Handle autoplay restriction
+    const playMusic = () => {
+      audio.play().catch(error => {
+        console.log('Autoplay prevented: ', error);
+      });
+    };
+
+    if (isMusicPlaying) {
+      playMusic();
+    } else {
+      audio.pause();
+    }
+
+    return () => {
+      audio.pause();
+    };
+  }, [isMusicPlaying]);
 
   const handleClick = () => {
     setMenuVisible(!isMenuVisible);
@@ -16,9 +39,22 @@ const Social = () => {
     setDonateVisible(!isDonateVisible);
   };
 
+  const toggleMusic = () => {
+    setMusicPlaying(!isMusicPlaying);
+  };
+
   return (
     <>
-      <div className="menu trans">
+
+
+     
+
+<div className="music_menu">
+          <a className="music" onClick={toggleMusic} title={isMusicPlaying ? "Pause" : "Play"}>
+            {isMusicPlaying ? <FaPause id="pause" /> : <FaPlay id="play" />}
+          </a>
+        </div>
+<div className="menu trans">
         <a className="" onClick={handleDonate}>
           <img src={Donate} alt="" />
         </a>
@@ -27,10 +63,10 @@ const Social = () => {
           className={`${isDonateVisible ? "hidden" : "label"}`}
           target="_blank"
         >
-         <span className="don">Donate</span>
+          <span className="don">Donate</span>
         </a>
-
       </div>
+
       <div className="menu">
         <a
           href="https://www.facebook.com/shrisaibabasansthantrustshirdi"
@@ -39,7 +75,6 @@ const Social = () => {
         >
           <FaFacebook id="facebook" />
         </a>
-        {/* <span className={` ${isMenuVisible ? "hidden" : "show"}`}> */}
         <a
           className={` ${isMenuVisible ? "hidden" : "show"}`}
           href="https://www.instagram.com/shreesaibabasansthantrust?utm_medium=copy_link"
@@ -47,7 +82,6 @@ const Social = () => {
         >
           <FaInstagram id="insta" />
         </a>
-        {/* </span> */}
         <a
           className={`${isMenuVisible ? "hidden" : "show"}`}
           href="https://twitter.com/ssstshirdi"
@@ -65,7 +99,10 @@ const Social = () => {
         <a className="" onClick={handleClick}>
           <BiSolidMessageSquareAdd id="soc" />
         </a>
+      
       </div>
+
+      
     </>
   );
 };
